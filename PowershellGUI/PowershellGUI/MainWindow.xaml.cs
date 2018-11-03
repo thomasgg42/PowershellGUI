ca21a6e7f12b048a001e1147fed20bd68b541a6b
Thomasgg <thomas.g@hotmail.no>
2018-11-03 13:56:26 +0100

    thought process ++

--- PowershellGUI/PowershellGUI/MainWindow.xaml.cs
+++ PowershellGUI/PowershellGUI/MainWindow.xaml.cs
@@ -8,32 +8,45 @@ namespace PowershellGUI
     /// 
 
     /*
-     * GUI tankemåte
-     * AD
-     *      psAdScript1.ps1
-     *          Les kommentarer i PS-fil, Description, Header og deretter X antall kommandolinjeargumenter som skal inn til PS-filen
-     *          Dette vil se slik ut: 
-     *          <#
-     *          Description = "beskrivelse"
-     *          Header = "Funksjonsnavn"
-     *          Output = "true"
-     *          [string]Username = "beskrivelse av CLI"
-     *          [int]SomeNumber = "beskrivelse av somenumber"
-     *          [bool]SomeBool = "beskrivelse av someBool"
-     *          #>
-     *          
-     *          Opprett antall input bokser tilsvarende antall kommando linje argumenter
-     *          Les beskrivelsen til hver kommandolinjeargument
-     *          Legg til beskrivelse tilhørende hver enkelt input boks
-     *          
-     *      psADScript2.ps1
-     *          ...
-     *          
-            psADScript3.ps1
-            ...
-     *      
-     * Exchange
+     * Mappe-struktur filplassering lese fra en settings-fil
+     * slik at vi kan flytte mappestrukturen 
      * 
+     * GUI tankemåte
+     * 
+     * AD (hardkodet, mappe)
+     *      MappevValg (Dynamisk antall mapper)
+     *          psAdScript1.ps1
+     *              Les kommentarer i PS-fil, Description, Header og deretter X antall kommandolinjeargumenter som skal inn til PS-filen
+     *              Dette vil se slik ut: 
+     *              <#
+     *              Description = "beskrivelse"
+     *              Header = "Funksjonsnavn"
+     *              Output = "true"
+     *              [string]Username = "beskrivelse av CLI"
+     *              [int]SomeNumber = "beskrivelse av somenumber"
+     *              [bool]SomeBool = "beskrivelse av someBool"
+     *              #>
+     *              
+     *              Opprett antall input bokser tilsvarende antall kommando linje argumenter
+     *              Les beskrivelsen til hver kommandolinjeargument
+     *              Legg til beskrivelse tilhørende hver enkelt input boks
+     *              
+     *          psADScript2.ps1
+     *              ...
+     *              
+                psADScript3.ps1
+
+                ...
+     *
+     * 
+     *      
+     * Exchange (hardkodet, mappe)
+     *      MappevValg (Dynamisk antall mapper)
+     *          exchangeScript1.ps1
+     *          exchangeScript2.ps1
+     * Skype (hardkodet, mappe)
+     *      Mappevalg (Dynamisk antall mapper)
+     *          skypeScript1.ps1
      */
     public partial class MainWindow : Window
         {
