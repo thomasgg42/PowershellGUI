5b8d7b0b0c9cfadf25fafeb24a5d56a731337366
Thomasgg <thomas.g@hotmail.no>
2018-11-03 13:41:18 +0100

    comments, thought process

--- PowershellGUI/PowershellGUI/MainWindow.xaml.cs
+++ PowershellGUI/PowershellGUI/MainWindow.xaml.cs
@@ -1,23 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PowershellGUI
     {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    /*
+     * GUI tankemåte
+     * AD
+     *      psAdScript1.ps1
+     *          Les kommentarer i PS-fil, Description, Header og deretter X antall kommandolinjeargumenter som skal inn til PS-filen
+     *          Dette vil se slik ut: 
+     *          <#
+     *          Description = "beskrivelse"
+     *          Header = "Funksjonsnavn"
+     *          Output = "true"
+     *          [string]Username = "beskrivelse av CLI"
+     *          [int]SomeNumber = "beskrivelse av somenumber"
+     *          [bool]SomeBool = "beskrivelse av someBool"
+     *          #>
+     *          
+     *          Opprett antall input bokser tilsvarende antall kommando linje argumenter
+     *          Les beskrivelsen til hver kommandolinjeargument
+     *          Legg til beskrivelse tilhørende hver enkelt input boks
+     *          
+     *      psADScript2.ps1
+     *          ...
+     *          
+            psADScript3.ps1
+            ...
+     *      
+     * Exchange
+     * 
+     */
     public partial class MainWindow : Window
         {
         public MainWindow()
