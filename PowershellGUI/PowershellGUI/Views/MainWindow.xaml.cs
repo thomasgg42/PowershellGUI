35819517afbfeb186db9f611a824db9ee6cb4376
Thomas <thomas.g@hotmail.no>
2018-11-12 07:55:22 +0100

    Removed test code

--- PowershellGUI/PowershellGUI/Views/MainWindow.xaml.cs
+++ PowershellGUI/PowershellGUI/Views/MainWindow.xaml.cs
@@ -23,10 +23,5 @@ namespace PowershellGUI.Views
 
             
             }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-            {
-            
-            }
         }
     }
