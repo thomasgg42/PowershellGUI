84353d5ccba3740cab3316414d7a93c3eccff7f3
Thomasgg <thomas.g@hotmail.no>
2018-12-10 16:00:09 +0100

    tmp

--- PowershellGUI/Modules/ActiveDirectory/Add Player To Exchange Mailbox.ps1
+++ PowershellGUI/Modules/ActiveDirectory/Add Player To Exchange Mailbox.ps1
@@ -2,12 +2,5 @@
 Description = "beskrivelse"
 Header = "Funksjonsnavn"
 Output = "True"
-[string]navn = "Brukerens navn"
-[int]alder = "Brukerens alder"
 #>
-param(
-	[string]$navn,
-	[int]$alder
-	)
-$msg = "Hei $navn, du er $alder år gammel."
-Write-Output $msg
+Get-Help
