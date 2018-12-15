6c9424724bd446a974c32785ddd5e2e438c7b245
Thomasgg <thomas.g@hotmail.no>
2018-12-15 22:20:43 +0100

    Refactoring start

--- PowershellGUI/PowershellGUI/Views/MainWindow.xaml.cs
+++ PowershellGUI/PowershellGUI/Views/MainWindow.xaml.cs
@@ -12,17 +12,9 @@ namespace PowershellGUI.Views
         public MainWindow()
             {
             InitializeComponent();
-            
             string devModulePath = @".\..\..\..\Modules"; // during developement
             string releaseModulePath = "Modules";         // release
             DataContext = new ViewModel(devModulePath);
-
-            // Scan antall mapper i ModulePath
-            // Opprett antall radio knapper ut ifra dette
-
-            // for hver valgte radio knapp
-            // scan antall mapper i mappen tilhørende valgt radio knapp
-            // populer dropdown med mappenes navn
             }
         }
     }
