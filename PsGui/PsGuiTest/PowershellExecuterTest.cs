75a034b1938902d9479fce6afa3d2c9fbf7a089f
Thomasgg <thomas.g@hotmail.no>
2018-12-17 15:56:42 +0100

    Testing

--- PsGui/PsGuiTest/PowershellExecuterTest.cs
+++ PsGui/PsGuiTest/PowershellExecuterTest.cs
@@ -19,9 +19,63 @@ namespace PsGuiTest
             {
             string modulePath = ".";
             PsExecViewModel psExec = new PsExecViewModel(modulePath);
+            Assert.AreEqual(true, DirectoryNameBrowser.IsEmpty());
+            Assert.AreEqual(false, psExec.IsScriptSelected);
             Assert.AreEqual(false, psExec.CanExecute(this));
             Assert.AreEqual("", psExec.SelectedScript);
             Assert.IsNotNull(psExec.ModulePath);
+            }
+
+        /// <summary>
+        /// When a category is chosen. The script shall be cleared from
+        /// the dropdown menu.
+        /// </summary>
+        [TestMethod]
+        public void CategoryChosenEmptyListTest()
+            {
+            string modulePath = ".";
+            PsExecViewModel psExec = new PsExecViewModel(modulePath);
+            psExec.DirectoryNameBrowser.Add("Active Directory");
+
+            Assert.AreEqual(false, psExec.IsScriptSelected);
+            Assert.AreEqual(true, psExec.ScriptVariables.IsEmpty());
+            }
+
+
+
+        /// <summary>
+        /// A script must be chosen before script input fields can be shown.
+        /// There shall be a script input field matching each of the defined
+        /// script command line arguments in the script header file.
+        /// </summary>
+        [TestMethod]
+        public void ScriptChosenEmptyInputFieldsTest()
+            {
+            string modulePath = ".";
+            PsExecViewModel psExec = new PsExecViewModel(modulePath);
+
+            // Script exists, empty input fields
+            psExec.SelectedScript = "SomeTestScript.ps1";
+            Assert.AreEqual(true, psExec.IsScriptSelected);
+            Assert.AreEqual(true, psExec.ScriptVariables.IsEmpty());
+
+
+            // Script not exists
+            psExec.SelectedScript = "";
+            Assert.AreEqual(false, psExec.IsScriptSelected);
+            }
+
+
+        /// <summary>
+        /// Before a powershell script is launched, all argument input
+        /// fields must contain legal values.
+        /// </summary>
+        [TestMethod]
+        public void ScriptArgumentTest()
+            {
+            string modulePath = ".";
+            PsExecViewModel psExec = new PsExecViewModel(modulePath);
+
             }
 
         /// <summary>
