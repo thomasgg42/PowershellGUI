3e9913b5a7d1501c70fea4b40dec05b5a4500386
Thomasgg <thomas.g@hotmail.no>
2019-01-16 16:53:54 +0100

    minor

--- PsGui/PsGui/Models/ActiveDirectoryInfo/ADUser.cs
+++ PsGui/PsGui/Models/ActiveDirectoryInfo/ADUser.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
-using System.DirectoryServices;
 
-namespace PsGui.ViewModels
+namespace PsGui.Models.ActiveDirectoryInfo
     {
-    class ActiveDirectoryInfo
+    class ADUser
         {
-        public string TabName { get; } = "AD Info";
-
         private string _phone;
         private string _principalName;
         private string _department;
@@ -25,15 +19,10 @@ namespace PsGui.ViewModels
         private string _extensionAttribute10;
         private string _extensionAttribute8;
 
-        private string _test;
-        private string _test2;
 
-
-
-        public ActiveDirectoryInfo()
+        public ADUser()
             {
-        //    _test = GetTest();
-          //  _test2 = GetTest2();
+
             }
 
         public string Title
@@ -48,7 +37,7 @@ namespace PsGui.ViewModels
                 _title = value;
                 }
             }
-        
+
         /// <summary>
         /// Sets or gets the mail.
         /// </summary>
@@ -183,100 +172,5 @@ namespace PsGui.ViewModels
                 _phone = value;
                 }
             }
-
-
-
-        public string GetTest()
-            {
-            /*
-            // Create a request for the URL. 		
-            WebRequest request = WebRequest.Create("https://www.nrk.no");
-            // If required by the server, set the credentials.
-            request.Credentials = CredentialCache.DefaultCredentials;
-            // Get the response.
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            // Display the status.
-   //         Console.WriteLine(response.StatusDescription);
-            // Get the stream containing content returned by the server.
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-
-            // Cleanup the streams and the response.
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-            return responseFromServer;
-           */
-            /*
-            // Parse site
-            var html = @"https://html-agility-pack.net/";
-            HtmlWeb web = new HtmlWeb();
-            var htmlDoc = web.Load(html);
-            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
-            return "Node Name: " + node.Name + "\n" + node.OuterHtml;
-            */
-            return "";
-            }
-
-        public string GetTest2()
-            {
-            string tmp = "";
-            // create new ldap connection
-            DirectoryEntry ldapCon = new DirectoryEntry("eikdc201.eikanett.eika.no", "h804602x", "jegkan14");
-            ldapCon.Path = "LDAP://OU=Customers,OU=SKALA,DC=EIKANETT,DC=eika,DC=no";
-            ldapCon.AuthenticationType = AuthenticationTypes.Secure;
-
-            DirectorySearcher search = new DirectorySearcher(ldapCon);
-            // search.Filter = "CN=Thomas Gundersen,OU=Users,OU=KO-ESS,OU=Konsern,OU=Customers,OU=SKALA,DC=EIKANETT,DC=eika,DC=no";
-            // search.Filter = "OU=KO-ESS,OU=Konsern,OU=Customers,OU=SKALA,DC=EIKANETT,DC=eika,DC=no";
-            search.Filter = "(samaccountname=H804602)";
-            SearchResult result = search.FindOne();
-            if (result != null)
-                {
-                ResultPropertyCollection fields = result.Properties;
-                foreach (String ldapField in fields.PropertyNames)
-                    {
-                    foreach (Object myColl in fields[ldapField])
-                        {
-                        tmp += ldapField + ": " + myColl.ToString() + "\n";
-                        }
-                    }
-                }
-            else
-                {
-                tmp = "fail";
-                }
-
-            return tmp;
-            }
-
-        public string Test
-            {
-            get
-                {
-                return _test;
-                }
-            set
-                {
-                _test = value;
-                }
-            }
-
-        public string Test2
-            {
-            get
-                {
-                return _test2;
-                }
-            set
-                {
-                _test2 = value;
-                }
-            }
-
         }
     }
