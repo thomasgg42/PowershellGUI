3090d597644e874c0ac86fbefde02f2b9093e14c
Thomas <thomas.g@hotmail.no>
2019-01-26 16:20:31 +0100

    Config file

--- PsGui/PsGui/ViewModels/MainViewModel.cs
+++ PsGui/PsGui/ViewModels/MainViewModel.cs
@@ -1,24 +1,128 @@
 ﻿using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 
 namespace PsGui.ViewModels
     {
     public class MainViewModel : ObservableObject
         {
-        private string powershellScriptModulePath = ".";
-        private string powershellScriptModuleFolderName = "Modules";
-
-        private string ADServerURI = "eikdc201.eikanett.eika.no";
-        private string ADldapPath = "LDAP://OU=Customers,OU=SKALA,DC=EIKANETT,DC=eika,DC=no";
-        private string ADpriviledgedUserName = "";
-        private string ADpriviledgedPassword = "";
-
+        private const string relConfigFilePath = "config.ini";
+   
         public ObservableCollection<object> Tabs { get; private set; }
+
+        private string powershellScriptFolderPath;
+        private string powershellScriptModuleFolderName;
+
+        private string adServerUri;
+        private string adLdapPath;
+        private string adPriviledgedUserName;
+        private string adPriviledgedPassword;
+
+        
 
         public MainViewModel()
             {
+            GetConfig();
             Tabs = new ObservableCollection<object>();
-            Tabs.Add(new PsExecViewModel(powershellScriptModulePath, powershellScriptModuleFolderName));
-        //    Tabs.Add(new ADInfoViewModel(ADServerURI, ADldapPath, ADpriviledgedUserName, ADpriviledgedPassword));
+            Tabs.Add(new PsExecViewModel(powershellScriptFolderPath, powershellScriptModuleFolderName));
+            //    Tabs.Add(new ADInfoViewModel(adServerUri, adLdapPath, adPriviledgedUserName, adPriviledgedPassword));
+            }
+
+        /// <summary>
+        /// Gets the config.ini contents and calls the function
+        /// responsible for parsing. If no config.ini file is found
+        /// a new one is generated with default values.
+        /// </summary>
+        private void GetConfig()
+            {
+            if(System.IO.File.Exists(relConfigFilePath))
+                {
+                string[] lines = System.IO.File.ReadAllLines(relConfigFilePath);
+                foreach (string line in lines)
+                    {
+                    // if not blank line or comment
+                    if ((line != null && line != "") && !(line.Trim().StartsWith("#")))
+                        {
+                        ParseConfig(line);
+                        }
+                    }
+                }
+            else
+                {
+                // Generate new file if it doesn't exist
+                GenerateNewConfigFile();
+                }
+
+            }
+
+        /// <summary>
+        /// Parses the configuration file contents provided
+        /// as an argument.
+        /// </summary>
+        /// <param name="line"></param>
+        private void ParseConfig(string line)
+            {
+            string key = line.Split('=')[0].Trim();
+            string value = ParseQuotationContent(line);
+
+            switch (key)
+                {
+                case "ps_rel_scriptfolder_path": powershellScriptFolderPath = value; break;
+                case "ps_scriptfolder_name": powershellScriptModuleFolderName = value; break;
+                case "ad_server_uri": adServerUri = value; break;
+                case "ad_ldap_path": adLdapPath = value; break;
+                case "ad_priviledged_username": adPriviledgedUserName = value; break;
+                case "ad_priviledged_password": adPriviledgedPassword = value; break;
+                default: break;
+                }
+            }
+
+        /// <summary>
+        /// Gets the content inside quotation marks in a string.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private string ParseQuotationContent(string line)
+            {
+            Regex regex = new Regex("\".*?\"");
+            var match = regex.Match(line);
+            string content = match.ToString();
+            int firstChar = 1;
+            int lastChar = content.Length;
+            content = content.Substring(firstChar);
+            return content.Substring(0, lastChar - 2);
+            }
+        
+        /// <summary>
+        /// Generates a config.ini file with default values.
+        /// </summary>
+        private void GenerateNewConfigFile()
+            {
+            string relativeFilePath = ".\\config.ini";
+            string[] contents =
+                {
+                "# This file MUST be placed at the same directory level",
+                "# as the executable. If not, the executable will",
+                "# generate a new, empty config.ini file.",
+                "",
+                "# Powershell executer relative file paths",
+                "ps_rel_scriptfolder_path = \".\"",
+                "ps_scriptfolder_name = \"Scripts\"",
+                "",
+                "# Active Directory info",
+                "ad_server_uri = \"\"",
+                "ad_ldap_path = \"\"",
+                "ad_priviledged_username = \"\"",
+                "ad_priviledged_password = \"\""
+                };
+            try
+                {
+                System.IO.File.WriteAllLines(relativeFilePath, contents);
+                }
+            catch (System.Exception e)
+                {
+                throw new PsGui.Models.PsGuiException("Could not create new config file. Check write permissions.", e.ToString());
+                }
+            
             }
         }
     }
