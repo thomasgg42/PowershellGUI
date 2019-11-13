eca35ac55895e729b3f7b78b0c35ca0dc84e6b69
Thomas <thomas.g@hotmail.no>
2019-11-13 21:20:09 +0100

    visual updates

--- PsGui/PsGui/ViewModels/PsExecViewModel.cs
+++ PsGui/PsGui/ViewModels/PsExecViewModel.cs
@@ -1098,7 +1098,11 @@ namespace PsGui.ViewModels
         {
             ScriptExecutionOutputCustom.Clear();
             ClearOutputStreams();
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             ClearProgressStreams();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
             ClearErrorStreams();
         }
 
