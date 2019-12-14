9b535eabbbdebe05043eda72b4d451d79799f937
Thomas <thomas.g@hotmail.no>
2019-12-14 21:36:58 +0100

    Radiobuttons implemented.

---
+++ PsGui/PsGui/Models/PowershellExecuter/RadiobuttonArgument.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsGui.Models.PowershellExecuter
+{
+    public class RadiobuttonArgument : ScriptArgument
+    {
+        private bool _isChecked;
+
+        /// Constructor.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="description"></param>
+        /// <param name="type"></param>
+        public RadiobuttonArgument(string key, string description, string type) : base(key, description, type)
+        {
+            // Checkboxes are unchecked by default
+            _isChecked = false;
+            base.InputValue = "false";
+        }
+
+        public bool IsChecked
+        {
+            get
+            {
+                return _isChecked;
+            }
+            set
+            {
+                _isChecked = value;
+                base.InputValue = _isChecked ? base.InputValue = "true" : base.InputValue = "false";
+            }
+        }
+    }
+}
